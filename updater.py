7bb9ab575a5d29f0cecc56e8095e7dc339eedd11
schuman-aaron <schuman-aaron@users.noreply.github.com>
2014-04-03 12:21:14 -0600

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,10 +1,5 @@
 import pygame
-
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
-TAN = (250, 230, 140)
+from sand_physics import sandphysics
 
 def update(the_grid, screen, update_pos):
     new_update_pos = list()
@@ -15,21 +10,22 @@ def update(the_grid, screen, update_pos):
             y=update_pos[i][1]
             #if marked for being sand
             if the_grid.pos[x][y].block == 'Sand':
-                pass
-                #update each individual positions of sand blocks if sand block
-                #alt_update_pos=sandphysics(the_grid, screen, update_pos[i])
-                #if alt_update_pos == None
-                    #remove_update_pos += update_pos[i]
-                #new_update_pos=alt_update_pos+new_update_pos
+                #update each individual positions of sand blocks
+                alt_update_pos=sandphysics(the_grid, screen, update_pos[i])
+                if alt_update_pos == None
+                    remove_update_pos += update_pos[i]
+                new_update_pos+=alt_update_pos
+            #don't bother updating nothing
             elif the_grid.pos[x][y].block == 'None':
                 remove_update_pos+=update_pos[i]
-            else: #elif the_grid.pos[x][y].block == 'Block', similar to above
-                pass
+            #don't bother updating block
+            elif the_grid.pos[x][y].block == 'Block':
+                remove_update_pos+=update_pos[i]
  
     #adds the new values that need to be update and
     #removes the values that intersect between update_pos and remove_update_pos           
     update_pos = new_update_pos+[val for val in update_pos if val not in remove_update_pos]
-    pygame.display.flip()
+    pygame.display.update()
     return update_pos
 
 
