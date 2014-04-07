01578368ff14d0ca9c25318f1c83b20c9ddcbb50
schuman-aaron <schuman-aaron@users.noreply.github.com>
2014-04-07 15:24:58 -0600

    Can no longer remove the walls

--- remove_block.py
+++ remove_block.py
@@ -12,7 +12,7 @@ def remove_blocks(mpos, the_grid, screen):
     x = mpos[0]
     y = mpos[1]
     #the position the user clicked on is not empty and within boundary
-    if the_grid.pos[x][y].block != 'None':
+    if the_grid.pos[x][y].block != 'None' and y>0 and x<len(the_grid.pos)-1 and x>0 and y<len(the_grid.pos[0])-1:
         #that position now contains nothing
         the_grid.pos[x][y].block = 'None'
 
