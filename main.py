857c47adba41065eaddc34d07c906c43ac3aed7e
schuman-aaron <schuman-aaron@users.noreply.github.com>
2014-04-06 23:10:54 -0600

    Reorganization of the code
    Changed the order of the code and indentation in order for the sand to be placed every tic regardless if an event has happened or not.

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ class grid:
     def __init__(self, screen_size,screen, GREY):
         y = screen_size[1]
         x = screen_size[0]
-        self.pos = list() 
-        #to get a certain position and attribute call for grid.pos[x][y].attribute  
+        self.pos = list()
+        #to get a certain position and attribute call for grid.pos[x][y].attribute
         for i in range(x):
             self.pos.append(list())
             for j in range(y):
@@ -64,47 +64,47 @@ update_pos = set()
 # -------- Main Program Loop -----------
 while not done:
     #Goes through for loop if the user did something
-    for event in pygame.event.get(): 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
             break
         mpos = pygame.mouse.get_pos()
         #check to see if the user left or right clicked
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:               
+            if event.button == 1:
                 mouse_down1 = 1
             elif event.button == 3:
                 mouse_down2 = 1
         #check to see if the user released the left mouse button
-        if event.type == pygame.MOUSEBUTTONUP and mouse_down1 == 1:
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:           
             mouse_down1 = 0
-        #left mouse button is being held down
-        if mouse_down1 == 1:
-            new_block = place_blocks(mpos, block_type, the_grid, screen)
-            if new_block:
-                update_pos.add(new_block)
-        #check to see if the user released the right mouse button
-        if event.type == pygame.MOUSEBUTTONUP and mouse_down2 == 1:
-            mouse_down2 = 0
-        #right mouse button is being held down
-        if mouse_down2 == 1:
-            remove_blocks(mpos, the_grid, screen)
-        #illegal mouse operation, reset left and right mouse button is down variables
-        #note: without this placing blocks would become slower and slower 
-        #each time the operation is executed
-        if mouse_down1==1 and mouse_down2 == 1:
-            mouse_down1 = 0
-            mouse_down2 = 0
-        #if the b button is pressed then change block type        
+        #if the b button is pressed then change block type
         if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
             if block_type == 'Sand':
                 block_type = 'Block'
             else:
                 block_type = 'Sand'
+        #check to see if the user released the right mouse button
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+            mouse_down2 = 0
+    #left mouse button is being held down
+    if mouse_down1 == 1:
+        new_block = place_blocks(mpos, block_type, the_grid, screen)
+        if new_block:
+            update_pos.add(new_block)
+        #right mouse button is being held down
+    if mouse_down2 == 1:
+        remove_blocks(mpos, the_grid, screen)
+        #illegal mouse operation, reset left and right mouse button is down variables
+        #note: without this placing blocks would become slower and slower
+        #each time the operation is executed
+    if mouse_down1==1 and mouse_down2 == 1:
+        mouse_down1 = 0
+        mouse_down2 = 0
     #update function
-    update(the_grid, screen, update_pos)           
+    update(the_grid, screen, update_pos)
 
     # Limit to 60 frames per second
-    clock.tick(600)
+    clock.tick(60)
 
 pygame.quit
