36b8facbc4421c555cfc3c1e84b624046dcc15ba
Hanson Le <sonnah4c65@users.noreply.github.com>
2014-04-09 23:15:29 -0600

    Finished commenting setcolor

--- setcolor.py
+++ setcolor.py
@@ -6,9 +6,11 @@ def set_color(screen, color, point):
     four pixels.
     """
     
+    # multiply to find the pixel location(assuming a tile has 2 pixel width)
     x = point[0] * 2
     y = point[1] * 2
     
+    # loop and set color
     for i in range(2):
         for j in range(2):
             screen.set_at((x + i, y + j), color)
