450f931af97a69f690c5d7ef574aefa7e4db9dfe
Hanson Le <sonnah4c65@users.noreply.github.com>
2014-04-09 23:32:35 -0600

    Finished commenting check blocks

--- check_blocks.py
+++ check_blocks.py
@@ -1,9 +1,17 @@
 def check_above(point, grid):
-    something_there = []
-    x = point[0] - 1
-    y = point[1] - 1
+    """
+    Given a block called 'point' in 'grid', the function determines if any of the
+    3 blocks above 'point' have a block type that is movable ("Sand", "Stone", etc).
+    Returns a list that contains the coordinates for the movable blocks.
+    """
+    movable_block = []
+    
+    x = point[0] - 1  # block to the left of 'point'
+    y = point[1] - 1  # block above 'point'
+    
+    # loop and add the coordinates to a list if the block type isn't 'None' or 'Block'
     for i in range(3):
         block_type = grid.pos[x + i][y].block
         if block_type != 'None' and block_type != 'Block':
-            something_there.append((x + i, y))
-    return something_there
+            movable_block.append((x + i, y))
+    return movable_block
