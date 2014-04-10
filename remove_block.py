40aef50b246450f38f473d27213c23181efb8ce9
Hanson Le <sonnah4c65@users.noreply.github.com>
2014-04-09 23:52:55 -0600

    finished commenting remove blocks

--- remove_block.py
+++ remove_block.py
@@ -10,18 +10,28 @@ TAN = (250, 230, 140)
 GREY = (200,200,200)
 
 def remove_blocks(mpos, the_grid, screen):
+    """
+    Remove the block at 'mpos' from 'the_grid'.
+    """
+    
+    # coordinates of block
     x = mpos[0]
     y = mpos[1]
 
+    # exit function if we try to remove a block on the border of the screen
     if x >= len(the_grid.pos) - 1 or y >= len(the_grid.pos[0]) - 1:
         return None
-    #the position the user clicked on is not empty and within boundary
+        
+    # the position the user clicked on is not empty and within boundary
     if the_grid.pos[x][y].block != 'None' and y>0 and x>0:
+        
         #that position now contains nothing
         the_grid.pos[x][y].block = 'None'
 
         #set density
         the_grid.pos[x][y].density = 0
+        
         #change color
         set_color(screen, BLACK, (x,y))
+        
         return check_above((x, y), the_grid)
