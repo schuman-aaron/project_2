f27f4b79e6f12bb7c955888401548f05d563708f
Hanson Le <sonnah4c65@users.noreply.github.com>
2014-04-09 23:40:38 -0600

    Finished commenting setcolor

--- setcolor.py
+++ setcolor.py
@@ -1,9 +1,16 @@
 import pygame
 
 def set_color(screen, color, point):
+    """
+    Change the color of a block by looping to change the color of the
+    four pixels.
+    """
+    
+    # multiply to find the pixel location(assuming a block has 2 pixel width)
     x = point[0] * 2
     y = point[1] * 2
     
+    # loop and set color
     for i in range(2):
         for j in range(2):
             screen.set_at((x + i, y + j), color)
